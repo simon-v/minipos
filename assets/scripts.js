dfcee1f8f791d276c45dbab7f2cce3c9692665a6
Simon Volpert <simon@simonvolpert.com>
2018-02-09 22:47:16 +0200

    Add decimal dot handling for amount entry Add "auto_cents" option for the previous input method Remove "multiplier" option Change amount box input type to "text" with accepted regex Add independent input validation for amount box

--- assets/scripts.js
+++ assets/scripts.js
@@ -24,6 +24,7 @@ function jsCheckReady(code) {
 // Form button controls
 function textAppend(value) {
 	document.getElementById("amountbox").value += value;
+	validateInput();
 	return_timer = 0;
 }
 function textRemove() {
@@ -32,13 +33,55 @@ function textRemove() {
 	field.value = value.substring(0, value.length - 1);
 	return_timer = 0;
 }
+function addCents() {
+	if ( document.getElementById("cents").value == "00" ) {
+		textAppend(0);
+		textAppend(0); // To allow validation to trigger
+	}
+	else {
+		var field = document.getElementById("amountbox"),
+		value = field.value;
+		if ( !~ value.indexOf(".") ) {
+			field.value = value + ".";
+		}
+		validateInput();
+	}
+	return_timer = 0;
+}
+
+// Form input validation
+function validateInput() {
+	var field = document.getElementById("amountbox"),
+	value = field.value;
+	// Decimal number leading zero
+	if ( value[0] == "." ) {
+		field.value = "0" + value;
+	}
+	// Two decimal places
+	else if ( ~ value.indexOf(".") && value.length - value.indexOf(".") > 3 ) {
+		textRemove();
+	}
+	// Leading zeroes
+	else if ( value.length > 1 && value[0] == "0" && value[1] != "." ) {
+		field.value = value.substring(1, value.length);
+	}
+}
 
 // Add the tax to the entered amount
 function addTax() {
 	var field = document.getElementById("amountbox"),
 	percents = document.getElementById("percents").innerHTML,
-	tax = percents.substring(0, percents.length - 1) / 100 + 1;
-	field.value = (field.value * tax).toFixed(0);
+	tax = percents.substring(0, percents.length - 1) / 100.0 + 1;
+	if ( document.getElementById("cents").value == "." ) {
+		var decimals = 2;
+	}
+	else {
+		var decimals = 0;
+	}
+	field.value = (field.value * tax).toFixed(decimals);
+	if ( field.value.substring(field.value.length - 3, field.value.length) == ".00" ) {
+		field.value = field.value.substring(0, field.value.length - 3);
+	}
 	return_timer = 0;
 }
 
