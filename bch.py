simon-v/minipos

commit 1c627fa6366a4a0aabbb8847b4aebe321cca6396

Move explorer error rate tracking to get_balance

bch.py
@@ -104,7 +104,6 @@ explorers = [
 random.seed()
 random.shuffle(explorers)
 for i in range(len(explorers)):
-	explorers[i]['errors'] = 0
 	explorers[i]['name'] = '.'.join(explorers[i]['url'].split('/')[2].split('.')[-2:])
 for i in range(len(exchanges)):
 	exchanges[i]['name'] = '.'.join(exchanges[i]['url'].split('/')[2].split('.')[-2:])
@@ -220,6 +219,8 @@ verify     (bool) the results should be verified with another explorer'''
 	server = None
 	results = []
 	while True:
+		if 'errors' not in explorers[0]:
+			explorers[0]['errors'] = 0
 		# Cycle to the next server
 		explorers.append(server)
 		server = explorers.pop(0)

commit e39ea1bb7ef32f645cdb2b29767359d8eae49480

Fix error rate tracking broken in commit 1c627fa

bch.py
@@ -195,11 +195,12 @@ confirmed_only  (bool) ignore servers without a separate unconfirmed balance'''
 	server = None
 	results = []
 	while True:
-		if 'errors' not in explorers[0]:
-			explorers[0]['errors'] = 0
 		# Cycle to the next server
 		explorers.append(server)
 		server = explorers.pop(0)
+		# Populate server error count if necessary
+		if server is not None and 'errors' not in server:
+			server['errors'] = 0
 		# If the end of the server list was reached without a single success, assume a network error
 		if server is None:
 			for i in range(len(explorers)):